0556c8bdb10e24787c9a7d0befa2d20e35b923be
Anuj Negi <anuj5negi5@gmail.com>
2021-07-22 02:09:49 +0530

    changed himani ma'am pic. Added remaining events. Connected to netlify

--- src/components/Events/EventsData.js
+++ src/components/Events/EventsData.js
@@ -1,39 +1,59 @@
 // import images
-import eventImage1 from "../../assets/images/Events/gcp.jpg";
-import eventImage2 from "../../assets/images/Events/weekoflearning.jpg";
-import eventImage3 from "../../assets/images/Events/hackfest.jpg";
-import eventImage4 from "../../assets/images/Events/ASJ.jpg";
-import eventImage5 from "../../assets/images/Events/mlevent.jpg";
+import gcp from "../../assets/images/Events/gcp.jpg";
+import weekOfLearning from "../../assets/images/Events/weekoflearning.jpg";
+import hackfest1 from "../../assets/images/Events/hackfest.jpg";
+import hackfest2 from "../../assets/images/Events/hackfest2.jpg";
+import hackfest3 from "../../assets/images/Events/hackfest3.jpg";
+import androidStudyJams from "../../assets/images/Events/ASJ.jpg";
+import mlEvent from "../../assets/images/Events/mlevent.jpg";
 
 export const EventsData = [
   {
-    id: 5,
+    id: 7,
     cardTitle: "ML Workshop",
     cardSubtitle: "3 Jul 2021 - 6 Jul 2021",
     cardText:
-      "This workshop gives you the gist of python basics, algorithms used in machine learning and how to move forward in this domain. It is a 4-day hourly workshop. A competition planned at the end of the workshop for all the young minds to win some exciting goodies.",
+      "This workshop gives you the gist of python basics, algorithms used in machine learning and how to move forward in this domain. It is a 4-day hourly workshop. A competition planned at the end of the workshop for all the young minds.",
     cardHref:
       "https://dsc.community.dev/events/details/developer-student-clubs-ajay-kumar-garg-engineering-college-presents-ml-workshop-by-dsc-akgec/",
-    eventImg: eventImage5,
+    eventImg: mlEvent,
   },
   {
-    id: 4,
+    id: 6,
     cardTitle: "Android Study Jams",
     cardSubtitle: "19 Dec - 23 Dec 2020",
     cardText:
-      "DSC-AKGEC brought 'Android Study Jams'. A chance for people who are new to android to learn how to build Android apps in Kotlin by following an online curriculum together with a study group. The workshop was for people who had prior experience in programming in any language. ",
+      "DSC-AKGEC brought 'Android Study Jams'. A chance for people who are new to android to learn how to build Android apps in Kotlin by following an online curriculum. The workshop was for people who had prior experience in programming. ",
     cardHref:
       "https://dsc.community.dev/events/details/developer-student-clubs-ajay-kumar-garg-engineering-college-presents-android-study-jams-prior-programming-experience/",
-    eventImg: eventImage4,
+    eventImg: androidStudyJams,
+  },
+  {
+    id: 5,
+    cardTitle: "Hackfest 3",
+    cardSubtitle: "24 Oct 2020",
+    cardText:
+      "DSC AKGEC presented a YouTube live Session, with Mr. Govind Goel on Youtube. He is a GSoC'20 mentor at DIAL. The event focused on the 'Basics of Open Source'.The session entailed basics of GitHub and the relevance of open source.",
+    cardHref: "https://www.instagram.com/p/CGmu-1gD7y5/",
+    eventImg: hackfest3,
+  },
+  {
+    id: 4,
+    cardTitle: "Hackfest 2",
+    cardSubtitle: "4 Oct 2020",
+    cardText:
+      "An informative and interactive session on Getting Started with Open Source by Mr. Anuj Garg, founder of Code for Cause and Open source Outreach and Advocate at JBoss Community. The session was live on YouTube on 4th of October at 5pm.",
+    cardHref: "https://www.instagram.com/p/CF36VjWDOSd/",
+    eventImg: hackfest2,
   },
   {
     id: 3,
-    cardTitle: "Hackfest",
+    cardTitle: "Hackfest 1",
     cardSubtitle: "2 Oct 2020",
     cardText:
-      "This session was based on Introduction to Cybersecurity and CFTs by Mr. Sunny Mishra, one of the founding members and CTF player and works as a reverse engineer and exploit developer.This event was brought by DSC AKGEC in collaboration with DSC IGDTUW, DSC Indraprastha College for Women and DSC PGDAV.",
+      "This session was based on Introduction to Cybersecurity and CFTs by Mr. Sunny Mishra, one of the founding members and CTF player and works as a reverse engineer and exploit developer.",
     cardHref: "https://www.instagram.com/p/CFzu9tsjF7L/",
-    eventImg: eventImage3,
+    eventImg: hackfest1,
   },
   {
     id: 2,
@@ -42,7 +62,7 @@ export const EventsData = [
     cardText:
       "An intensive week- long learning program from Propagate An online learning platform to learn Web Development from absolute basics and build the first project. Amazon vouchers, certificates for completion and goodies were provided.",
     cardHref: "https://www.instagram.com/p/B9_znonD7V5/",
-    eventImg: eventImage2,
+    eventImg: weekOfLearning,
   },
   {
     id: 1,
@@ -51,6 +71,6 @@ export const EventsData = [
     cardText:
       "Contained 3 tracks: App and Web Development, Data Science, Machine Learning. Each track had 5 quests and 1 quest to be completed within a week. The eligible entries were evaluated by Google and successful entries received hoodies, badges, pens, stickers and swags.",
     cardHref: "https://www.instagram.com/p/B3OicsfDTRE/",
-    eventImg: eventImage1,
+    eventImg: gcp,
   },
 ];
